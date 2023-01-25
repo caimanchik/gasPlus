cdef1e80758f91ddf059ee502729d5ff159a8416
caiman <shimyshimya@mail.ru>
2023-01-25 16:47:23 +0500

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,7 @@ import * as functions from "./modules/functions.js"
 
 functions.isWebp();
 functions.selectChange()
+
+let slider = new functions.Slider()
+
+
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -29,3 +29,107 @@ export function selectChange() {
         })
     })
 }
+
+export class Slider {
+    
+    constructor() {
+        this.selected = 0
+        this.slides = Array.from(document.querySelector('.slider__slides').children);
+        this.slider = document.querySelector('.slider__slider');
+        this.navigation = document.querySelector('.slider__navigation');
+        
+        this.initNavigation()
+        this.initArrows()
+        this.initSlider()
+    }
+    
+    initSlider() {
+        this.slider.append(this.createElement(['slider__slide-visible', 'front'], this.slides[this.selected]))
+        this.slider.append(this.createElement(['slider__slide-visible', 'center']))
+        this.slider.append(this.createElement(['slider__slide-visible', 'back']))
+        this.slider.append(this.createElement(['slider__slide-visible', 'next']))
+    }
+    
+    initNavigation() {
+        this.navigates = []
+        
+        this.slides.forEach((e, i) => {
+            let nav = this.createElement(['slider__navigate'])
+            this.navigates.push(nav)
+            this.navigation.append(nav)
+        })
+        
+        this.navigates.forEach((e, i) => e.addEventListener('click', event => {
+            this.navigates.forEach(nav => nav.classList.remove('selected'))
+            e.classList.add('selected')
+            if (Math.abs(this.selected % this.slides.length) !== i) {
+                this.selected = i
+                this.changeSlide()
+            }
+        }))
+        
+        this.changeNav()
+    }
+    
+    changeNav() {
+        this.navigates.forEach((e, i) => {
+            if (i !== Math.abs(this.selected % this.slides.length)) {
+                e.classList.remove('selected')
+                return
+            }
+            e.classList.add('selected')
+        })
+    }
+    
+    createElement(classes, content = '') {
+        let e = document.createElement('div')
+        e.classList.add(...classes)
+        e.append(content)
+        return e
+    }
+    
+    initArrows() {
+        let next = document.querySelector('.slider__arrow-next');
+        let prev = document.querySelector('.slider__arrow-prev');
+
+        next.addEventListener('click', e => {
+            this.selected++
+            this.changeSlide()
+            this.changeNav()
+        })
+        prev.addEventListener('click', e => {
+            this.selected--
+            if (this.selected < 0) 
+                this.selected = this.slides.length - 1
+            this.changeSlide()
+            this.changeNav()
+        })
+    }
+    
+    changeSlide() {
+        let center = this.slider.querySelector('.center')
+        let front = this.slider.querySelector('.front')
+        let back = this.slider.querySelector('.back')
+        let next = this.slider.querySelector('.next')
+        center.append(this.slides[Math.abs(this.selected) % this.slides.length])
+        
+        front.classList.add('out')
+        front.classList.remove('front')
+        
+        center.classList.add('front')
+        center.classList.remove('center')
+        
+        back.classList.add('center')
+        back.classList.remove('back')
+        
+        next.classList.add('back')
+        next.classList.remove('next')
+        
+        this.slider.append(this.createElement(['slider__slide-visible', 'next']))
+        
+        let t = setTimeout(() => {
+            front.remove()
+            clearTimeout(t)
+        }, 500);
+    }
+}
